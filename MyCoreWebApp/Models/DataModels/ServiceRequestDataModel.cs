244abdc7f8df871dca51273bc18715ea965d1464
delaneyn1 <ndelaney17@gmail.com>
2019-06-04 15:48:38 -0400

    New data model added for service request. Generic starter template, still need data annotations, controller, and view.

---
+++ MyCoreWebApp/Models/DataModels/ServiceRequestDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCoreWebApp.Models.DataModels
+{
+    public class ServiceRequestDataModel
+    {
+        public List<string> ServiceRequesttype { get; set; }
+        public string ServiceRequestDescription { get; set; }
+        public Datetime IdealRequestDate { get; set; }
+        public bool CustomerConsent { get; set; }
+        public List<string> PreferredContactMethod { get; set; }
+    }
+}
