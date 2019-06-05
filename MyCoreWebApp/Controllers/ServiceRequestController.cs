0e5df36ef1822f822cffcf1fdfb7feb550c678c5
Nick Delaney <ndelaney17@gmail.com>
2019-06-05 12:53:00 -0400

    Add files via upload
    Manually upload service request controller

---
+++ MyCoreWebApp/Controllers/ServiceRequestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//Used for customers to submit work orders
+namespace MyCoreWebApp.Controllers
+{
+    public class ServiceRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
