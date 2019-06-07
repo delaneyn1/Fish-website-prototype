deba1672aa8b796f4029615e4a803be27296986d
Nick Delaney <ndelaney17@gmail.com>
2019-06-07 19:59:10 -0400

    I changed the create cs and added the request drop down with mods

--- MyCoreWebApp/Models/DataModels/ServiceRequestDataModel.cs
+++ MyCoreWebApp/Models/DataModels/ServiceRequestDataModel.cs
@@ -13,23 +13,33 @@ namespace MyCoreWebApp.Models.DataModels
     //BUG: Datetime default value is invalid when rendoring create view. 
     public class ServiceRequestDataModel
     {
-       //Gets called when instantiating this model to prepopulate list for the view.
+        //Gets called when instantiating this model to prepopulate list for the view.
         public ServiceRequestDataModel()
         {
             List<RequestDefinitions> mylist = new List<RequestDefinitions>();
-            mylist.Add(new RequestDefinitions { RequestTypeId = 1, RequestName = "General Checkup", RequestDescription = "Standard check up includes but not limited to water sampling for chemical balance, equipment review, refilling auto feeders etc."});
+            mylist.Add(new RequestDefinitions { RequestTypeId = 1, RequestName = "General Checkup", RequestDescription = "Standard check up includes but not limited to water sampling for chemical balance, equipment review, refilling auto feeders etc." });
             mylist.Add(new RequestDefinitions { RequestTypeId = 2, RequestName = "Maintenance / Repair", RequestDescription = "Troubleshooting foggy water, equipment repair etc." });
-            mylist.Add(new RequestDefinitions { RequestTypeId = 3, RequestName = "Cleaning", RequestDescription = "Glass cleaning, water recycling, any other things that can be done to make the tank looking fresh and clean like its brand new."});
+            mylist.Add(new RequestDefinitions { RequestTypeId = 3, RequestName = "Cleaning", RequestDescription = "Glass cleaning, water recycling, any other things that can be done to make the tank looking fresh and clean like its brand new." });
             mylist.Add(new RequestDefinitions { RequestTypeId = 4, RequestName = "Equipment", RequestDescription = "Troubleshooting foggy water, equipment repair etc." });
-            mylist.Add(new RequestDefinitions { RequestTypeId = 5, RequestName = "Tank Mpdfication", RequestDescription = "This would be for cases like if you need a tank moved or you want to change to a different different size tank." });
+            mylist.Add(new RequestDefinitions { RequestTypeId = 5, RequestName = "Tank Modfication", RequestDescription = "This would be for cases like if you need a tank moved or you want to change to a different different size tank." });
             mylist.Add(new RequestDefinitions { RequestTypeId = 6, RequestName = "Other", RequestDescription = "You fill in the blank, this could be unique situations such as troubleshooting new unwanted noises from pumps or just general consulting for future enhancements." });
             RequestTypeInformation = mylist;
+
+            List<requestTypeOptions> Dropdown = new List<requestTypeOptions>();
+            Dropdown.Add(new requestTypeOptions { ID = 1, requestname = "General Checkup" });
+            Dropdown.Add(new requestTypeOptions { ID = 2, requestname = "Maintenence" });
+            Dropdown.Add(new requestTypeOptions { ID = 3, requestname = "Cleaning" });
+            Dropdown.Add(new requestTypeOptions { ID = 4, requestname = "Equipment" });
+            Dropdown.Add(new requestTypeOptions { ID = 5, requestname = "Tank Modification" });
+            Dropdown.Add(new requestTypeOptions { ID = 6, requestname = "Other" });
+            ServiceRequesttype = Dropdown;
         }
+        public Gender StudentGender { get; set; }
 
         //TODO: Populate list to form drop down controls.
         //TODO: Add Validation to properties using data annotations, value must be greater than 1 and required
         [DisplayName("Service Request Type")]
-        public List<string> ServiceRequesttype { get; set; }
+        public List<requestTypeOptions> ServiceRequesttype { get; set; }
 
         //TODO: Needs a min length validation added and display name
         public string ServiceRequestDescription { get; set; }
@@ -55,4 +65,22 @@ namespace MyCoreWebApp.Models.DataModels
         [Display(Name = "Description")]
         public string RequestDescription { get; set; }
     }
+    //for the request ddl 
+    public class requestTypeOptions
+    {
+        //value of the type selected
+        public int ID { get; set; }
+        public string requestname { get; set; }
+    }
+    public enum Gender
+    {
+        GeneralCheckup,
+        Maintenence,
+        Cleaning,
+        Equipment,
+        TankModification,
+        other,
+
+    }
+
 }
